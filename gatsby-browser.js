41efee87b0b93cbaac2b00f851f261c8fccdaab5
John Pham <john@highlight.run>
2021-07-28 13:24:14 -0700

    Cleanup code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,9 @@
 const Highlight = require("highlight.run");
 
-const PACKAGE_NAME = "@highlight-run/gatsby-plugin-highlight";
-
 exports.onClientEntry = (_, pluginOptions) => {
   const { orgID, plugins, ...options } = pluginOptions;
 
-  if (orgID === undefined) {
-    throw new Error(`Invalid options for "${PACKAGE_NAME}": `);
-  }
-
-  Highlight.H.init(orgID, options);
+  Highlight.H.init(orgID, options.options);
 
   // @ts-ignore
   window.H = Highlight.H;
